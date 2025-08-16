fcc777432dcecba2e74222b8d9c91c8ef5799a80
Carlos Berdejo <carlosberdejopar@gmail.com>
2025-08-15 23:03:30 -0500

    first commit

---
+++ src/main/java/com/clinicAnimal/system_vet_clinica/SystemVetClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.clinicAnimal.system_vet_clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemVetClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemVetClinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicAnimal/system_vet_clinica/SystemVetClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicAnimal.system_vet_clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemVetClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
